67ea1ccb93a498fb3201e2684f4c84b630da9ae6
Jesser Sison <jesser.sison@live.com>
2020-03-03 21:24:32 -0800

    feat: add navigation bar

--- src/components/shared/navbar/navbar.js
+++ src/components/shared/navbar/navbar.js
@@ -5,42 +5,52 @@ import './navbar.css'
 export default function Navbar(props) {
   return (
     <div class="navbar">
-      <img class="logo" alt="measuremusic" src="/images/measuremusic.png" />
+      <a href="/#"><img class="logo" alt="measuremusic" src="/images/measuremusic.png" /></a>
 
       <ul class="navlinks">
-        <li><a href="/#">Dashboard</a></li>
-        <li>
-          <a href="/#">Charts</a>
-          <i class="fas fa-search" />
+        <li class="flex-container inactive-link li-sp-lg"><a class="flex-link navlink" href="/#">Dashboard</a></li>
+        <li class="li-sp-lg">
+          <a class="flex-container navlink" href="/#">
+            <span class="flex-link">Charts</span>
+            <i class="fas fa-chevron-up fa-sm" />
+          </a>
         </li>
 
         <ul class="charts-links">
-          <li>
-            <i class="fas fa-search" />
-            <a href="/#">Hits50</a>
+          <li class="li-sp-sm">
+            <a class="flex-container navlink" href="/#">
+              <i class="fas fa-chevron-right fa-sm selected-link" />
+              Hits50
+            </a>
           </li>
-          <li><a href="/#">Alternative</a></li>
-          <li><a href="/#">AC</a></li>
-          <li><a href="/#">Country</a></li>
-          <li><a href="/#">Active Rock</a></li>
-          <li><a href="/#">Smooth Jazz</a></li>
+          <li class="flex-container li-sp-sm inactive-link"><a class="charts-link flex-link" href="/#">Alternative</a></li>
+          <li class="flex-container li-sp-sm inactive-link"><a class="charts-link flex-link" href="/#">AC</a></li>
+          <li class="flex-container li-sp-sm inactive-link"><a class="charts-link flex-link" href="/#">Country</a></li>
+          <li class="flex-container li-sp-sm inactive-link"><a class="charts-link flex-link" href="/#">Active Rock</a></li>
+          <li class="flex-container li-sp-sm inactive-link"><a class="charts-link flex-link" href="/#">Smooth Jazz</a></li>
         </ul>
 
-        <li>
-          <a href="/#">Artist Insights</a>
-          <i class="fas fa-search" />
+        <li class="li-sp-lg">
+          <a class="inactive-link flex-container navlink" href="/#">
+            <span class="flex-link">Artist Insights</span>
+            <i class="fas fa-chevron-down fa-sm" />
+          </a>
         </li>
-        <li>
-          <a href="/#">Song Insights</a>
-          <i class="fas fa-search" />
+        <li class="li-sp-lg">
+          <a class="inactive-link flex-container navlink" href="/#">
+            <span class="flex-link">Song Insights</span>
+            <i class="fas fa-chevron-down fa-sm" />
+          </a>
         </li>
-        <li><a href="/#">Label Insights</a></li>
-        <li>
-          <a href="/#">Station Insights</a>
-          <i class="fas fa-search" />
+        <li class="flex-container inactive-link li-sp-lg"><a class="flex-link navlink" href="/#">Label Insights</a></li>
+        <li class="li-sp-lg">
+          <a class="inactive-link flex-container navlink" href="/#">
+            <span class="flex-link">Station Insights</span>
+            <i class="fas fa-chevron-down fa-sm" />
+          </a>
         </li>
-        <li><a href="/#">Market View</a></li>
-        <li><a href="/#">Compare</a></li>
+        <li class="flex-container inactive-link li-sp-lg"><a class="flex-link navlink" href="/#">Market View</a></li>
+        <li class="flex-container inactive-link li-sp-lg"><a class="flex-link navlink" href="/#">Compare</a></li>
       </ul>
     </div>
   )
